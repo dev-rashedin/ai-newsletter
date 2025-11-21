217129a079021d81b4172488d775bde40a542a92
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-21 12:32:35 +0600

    server.ts updated

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,10 +1,32 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export function createClient() {
-  return createBrowserClient(
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value }) =>
+              cookieStore.set(name, value)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
   );
 }
 
-export const supabase = createClient();
+
+export const supabase = await createClient();
